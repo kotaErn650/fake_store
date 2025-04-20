576235647458db09f1ed5817b12263e93e802667
joseLopez <kota012011@gmail.com>
2025-04-20 09:26:24 -0500

    checkout cardProduc

--- src/app/pages/products/product-list/product-list.component.ts
+++ src/app/pages/products/product-list/product-list.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { CardProductComponent } from "../../../componets/card-product/card-product.component";
 
 @Component({
   selector: 'app-product-list',
-  imports: [],
+  imports: [CardProductComponent],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
